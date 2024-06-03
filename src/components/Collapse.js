92333f25290892c6492838d9e71b769f313e0ab7
AlmostH <30635016+AlmostH@users.noreply.github.com>
2024-06-03 15:35:14 +0200

    création du menu collapse

---
+++ src/components/Collapse.js
@@ -0,0 +1,39 @@
+import { useState, useEffect, useRef } from "react";
+import arrow from "../images/arrow.jpg";
+function Collapse(props) {
+    const [open, setOpen] = useState(false);
+    const [height, setHeight] = useState();
+
+    const refHeight = useRef()
+
+    useEffect(() => {
+        setHeight(`${refHeight.current.scrollHeight}px`)
+    }, [])
+
+    function handleClick() {
+        setOpen(!open)
+    }
+
+    return (
+        <div className="collapse">
+
+            <button className="collapse__title">
+                <h2>{props.label}</h2>
+                <img src={arrow} alt="Détails" onClick={handleClick}
+                    className={`btn ${open ? "btn-active" : ""}`}
+                    id="chevron"
+                />
+            </button>
+            <div
+                className={open ? "collapse__text visible" : "collapse__text"}
+                style={{ height: open ? `${height}` : "0px" }}
+                ref={refHeight}
+            >
+                <div aria-hidden={open ? "true" : "false"} className="p">
+                    {props.children}
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Collapse
--- src/pages/Apropos.js
+++ src/pages/Apropos.js
@@ -1,6 +1,9 @@
 import Header from "../components/Header";
 import BannerAbout from "../components/BannerAbout";
 import Footer from "../components/Footer";
+import Collapse from "../components/Collapse";
+
+
 
 
 
@@ -11,6 +14,42 @@ export default function Apropos(){
 
         <Header />
         <BannerAbout />
+      
+        <Collapse label="Fiabilité">
+                <p>
+                    Les annonces postées sur Kasa garantissent une fiabilité totale.
+                    Les photos sont conformes aux logements, et toutes les informations
+                    sont régulièrements vérifiées par nos équipes.
+                </p>
+            </Collapse>
+           
+            <Collapse label="Respect">
+                <p>
+                    La bienveillance fait partie des valeurs fondatrices de Kasa.
+                    Tout comportement discriminatoire ou de perturbation du
+                    voisinage entrainera une exclusion de notre plateforme.
+                </p>
+            </Collapse>
+         
+            <Collapse label="Service">
+                <p>
+                    La bienveillance fait partie des valeurs fondatrices de Kasa.
+                    Tout comportement discriminatoire ou de perturbation du
+                    voisinage entrainera une exclusion de notre plateforme.
+                </p>
+            </Collapse>
+          
+            <Collapse label="Sécurité">
+                <p>
+                    La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes
+                    que pour les voyageurs, chaque logement correspond aux critères
+                    de sécurité établis par nos services. En laissant une note aussi
+                    bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier
+                    que les standards sont bien respectés. Nous organisons également des
+                    ateliers sur la sécurité domestique pour nos hôtes.
+
+                </p>
+            </Collapse>
   
         <Footer />
         </>
