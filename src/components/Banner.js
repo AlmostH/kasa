490f68481be802819edc9ee628c207bf3edee325
AlmostH <30635016+AlmostH@users.noreply.github.com>
2024-06-12 08:54:35 +0200

    banner responsive

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,10 @@
 export default function Banner() {
 
       return (
-        <section className="banner_home">
+        <section className="banner_wrapper">
+          <div className="banner_home"> 
           <h2>Chez vous, partout et ailleurs</h2>
+          </div>
         </section>
       );
     
