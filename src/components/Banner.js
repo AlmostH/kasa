62db73a22000fb40753c5bd0a236e78a596dfaa9
AlmostH <30635016+AlmostH@users.noreply.github.com>
2024-06-21 17:27:12 +0200

    modif scss collapse & banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,9 @@
 export default function Banner() {
 
       return (
-        <section className="banner_wrapper">
-          <div className="banner_home"> 
-          <h2  className="banner_title">Chez vous, partout et ailleurs</h2>
+        <section className="banner">
+          <div className="banner__home"> 
+          <h2  className="banner__title">Chez vous, partout et ailleurs</h2>
           </div>
         </section>
       );
